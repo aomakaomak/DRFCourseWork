7b83297f672ab66fa043f09f1bc6bda2dec745d7
Andrei Makarov <aomak@yandex.ru>
2025-11-29 18:49:17 +0300

    реализованы эндпойты для привычек и пользвоателей, права доступа

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("habits.urls", namespace="habits")),
-    # path("", include("users.urls", namespace="users")),
+    path("", include("users.urls", namespace="users")),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserRegisterSerializer
+
+
+class RegisterView(APIView):
+    """
+    Эндпоинт регистрации нового пользователя.
+    Доступен без авторизации.
+    """
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
